e80f6a0930039bb5ccd978137e9c9bacda693dfa
Élcio M. Fernandes <elciomateusfernandes1@gmail.com>
2024-05-13 21:25:56 -0300

    add react to project

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import pandas as pd
-import random
-
-dataframe = pd.read_excel('spreadsheets/setor_economico_ajustado.xlsx')
-
-for index, value in enumerate(dataframe['VL_PGTO']):
-    if value < 10000 or value / 1e15 >= 1:
-        dataframe.at[index, 'VL_PGTO'] = round(random.uniform(10000, 80000), 2)
-        
-dataframe.to_excel('spreadsheets/setor_economico_ajustado2.xlsx', index=False)
+print("hello world")
